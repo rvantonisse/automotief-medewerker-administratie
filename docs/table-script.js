bdaf486176c628c4ce2cceb4598a64d79888d154
Roel Antonisse <roel.antonisse@gmail.com>
2022-09-27 16:56:53 +0200

    Update table-script
    Add translated date
    Add classes to rows and cells

--- docs/table-script.js
+++ docs/table-script.js
@@ -1,17 +1,14 @@
 // Script used to initially render the static table
 (function () {
-    const tableElement =
-        document.getElementById("table_employees");
+    const tableElement = document.getElementById("table_employees");
     const tableBody = document.createElement("tbody");
 
     const employeeTableData = context.staff
         .map((member) => {
             // Retrieve matching employee details
-            const memberEmploymentDetails =
-                context.employmentDetails.find(
-                    (employmentDetail) =>
-                        employmentDetail.id === member.id
-                );
+            const memberEmploymentDetails = context.employmentDetails.find(
+                (employmentDetail) => employmentDetail.id === member.id
+            );
 
             // Return a new object with all employee keys flattened for the table
             return {
@@ -23,24 +20,48 @@
         .sort((a, b) => by("id")(a, b));
 
     // Build the table body
-    tableBody.innerHTML = employeeTableData.reduce(
-        function createTableRow(result, member) {
-            return (
-                result +
-                `
-                <tr>
+    tableBody.innerHTML = employeeTableData.reduce(function createTableRow(
+        result,
+        member
+    ) {
+        const isInactive = !member.active;
+        const statusClass = ((status) => {
+            switch (status) {
+                case "aanwezig":
+                    return "present";
+
+                case "afwezig":
+                    return "absent";
+
+                case "buiten":
+                    return "out-of-office";
+
+                default:
+                    return "";
+            }
+        })(member.status);
+
+        return (
+            result +
+            `
+                <tr${isInactive ? ` class="inactive"` : ""}>
                     <td>${member.name} ${member.last_name}</td>
-                    <td>${member.id}</td>
+                    <td class="number">${member.id}</td>
                     <td>${member.function}</td>
-                    <td>${member.employed_since}</td>
-                    <td>${member.status}</td>
+                    <td>${new Date(member.employed_since).toLocaleDateString(
+                        "nl"
+                    )}</td>
+                    <td class="status"><span class="${statusClass}">${
+                member.status
+            }</span></td>
                     <td>${member.remark}</td>
-                    <td><span>€</span> ${member.salary}</td>
+                    <td class="number"><span>€</span> <span class="salary">${
+                        member.salary
+                    }</span></td>
                 </tr>`
-            );
-        },
-        ""
-    );
+        );
+    },
+    "");
 
     tableElement.append(tableBody);
 
